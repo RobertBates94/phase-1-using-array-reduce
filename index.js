ee97af23da6da1939ef3c9df867278d211671738
Robert <bates.to.infinity@gmail.com>
2021-11-22 08:41:36 -0600

    commit tree 35fba0dbec89cafe3e9f86b87c85dafd648a1fba

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-let totalBatteries = 0
-
-batteryBatches.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue
-}, 0)
+const totalBatteries = batteryBatches.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+)
